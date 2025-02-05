b556c44d3a07cf42b9d95acef3e123cf4903ea01
Luciano Alfredo <lucianalfred60@gmail.com>
2025-02-05 08:12:30 +0100

    hgh func

---
+++ misc/strict mode/strict.js
@@ -0,0 +1,28 @@
+function strictly(){
+    'use strict';
+    //
+}
+
+/*
+*   <Linting Tools
+Linting tools such as JS Lint,1 JS Hint,2 and ES Lint3 can be used to test the
+quality of JavaScript code, beyond simply using strict mode. They are designed to
+highlight any sloppy programming practices or syntax errors, and will complain
+if certain style conventions are not followed, such as how code is indented. They
+can be very unforgiving and use some opinionated coding conventions, such as
+not using the ++ and -- increment operators (in the case of JS Lint). Linting tools
+are also useful for enforcing a programming style guide. This is particularly
+useful when you are working in a team, as it ensures everybody follows the same
+conventions.
+It’s possible to add a linting tool as a text-editor plugin; this will then highlight
+any sloppy code as you type. Another option is to use an online linting tool that
+allows you to simply paste onto a page for feedback. Another option is to install
+linting software on your computer using npm. This can then be run as part of
+your workflow.
+Passing a lint test is no guarantee that your code is correct, but it will mean it will
+be more consistent and less likely to have problems.
+*
+*
+*
+*
+*/
